97938774c3c933ca63e86753f9663ee22f4e10cf
szz <2811597910@qq.com>
2020-04-20 11:40:34 +0800

    提交多个lombok包

--- src/main/java/com/etoak/HouseApp.java
+++ src/main/java/com/etoak/HouseApp.java
@@ -1,7 +1,13 @@
 package com.etoak;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * Created by 宋章志 on 2020/4/20.
  */
+@SpringBootApplication
 public class HouseApp {
+    public static void main(String[]args){
+    SpringBootApplication.run(HouseApp.class,args);
+    }
 }
